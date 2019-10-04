c4753138bee3ab2f3d120ba4bedd81cbf82b0732
unknown <merlinsn@gmail.com>
2019-10-03 22:21:32 -0300

    working async

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,16 @@ import org.apache.commons.lang3.SerializationUtils;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    private static final int SIZE = 32;
+
+    public static void main(String[] args) throws IOException, InterruptedException {
         Scanner ob = new Scanner(System.in);
 
         SerialPort comPort = SerialPort.getCommPort("COM5");
@@ -18,18 +22,49 @@ public class Main {
 
         System.out.println("input:");
         String input = ob.nextLine();
-        while(!input.equals("n")){
+        while (!input.equals("n")) {
             byte[] data = SerializationUtils.serialize(input);
-            //byte[] data = input.getBytes();
+
             System.out.println("El arreglo tiene largo: " + data.length);
             System.out.println("el Array: " + Arrays.toString(data));
-            outputStream.write(data);
-            outputStream.flush();
+
+            List<byte[]> listOfArrays = splitArray(data, SIZE);
+
+            for (byte[] array : listOfArrays) {
+                System.out.println("El sub arreglo tiene largo: " + array.length);
+                System.out.println("el sub Array: " + Arrays.toString(array));
+                outputStream.write(array);
+                outputStream.flush();
+            }
 
             System.out.println("input:");
             input = ob.nextLine();
         }
     }
 
+    private static List<byte[]> splitArray(byte[] original, int size) {
+        int modulus = original.length % size;
+        int numberOfArrays = (original.length / size) + 1;
+        List<byte[]> listOfArrays = new ArrayList<>(numberOfArrays);
 
+        if (numberOfArrays == 0) {
+            listOfArrays.add(original);
+            return listOfArrays;
+        }
+
+        for (int i = 0; i < numberOfArrays; i++) {
+            byte[] aux;
+            aux = new byte[size];
+            Arrays.fill(aux, (byte)-127);
+            if (i + 1 == numberOfArrays) {
+                System.arraycopy(original, size * i, aux, 0, modulus);
+            } else {
+                aux = new byte[size];
+                System.arraycopy(original, size * i, aux, 0, size);
+            }
+            listOfArrays.add(aux);
+        }
+
+        return listOfArrays;
+    }
 }
